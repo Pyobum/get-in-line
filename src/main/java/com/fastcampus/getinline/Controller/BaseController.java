f1e332644c41c5d542bc478295cc8759365b0ee7
min-38 <mindino03@gmail.com>
2023-12-22 14:12:33 +0900

    Add - View Controller 5가지 뷰 컨트롤러 작성

---
+++ src/main/java/com/fastcampus/getinline/Controller/BaseController.java
@@ -0,0 +1,19 @@
+package com.fastcampus.getinline.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class BaseController {
+
+    @GetMapping("/")
+    public String root() {
+        return "index";
+    }
+
+    @RequestMapping("/error")
+    public String error() {
+        return "error";
+    }
+}
